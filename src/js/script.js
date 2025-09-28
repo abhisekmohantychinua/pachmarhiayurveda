a9e86dc86e36c1b4cfa89bff54e983c51f135708
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2025-09-28 13:30:41 +0530

    - Added "Times New Roman" font - Added navbar and responsiveness - Modified vite `dev` to open host

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log("Loaded");
+const menuBtn = document.querySelector(".nav__menu");
+const navItems = document.querySelector(".nav__items");
+
+menuBtn.addEventListener("click", () => {
+  const isOpened = navItems.getAttribute("data-opened") === "true";
+  navItems.setAttribute("data-opened", !isOpened);
+  menuBtn.classList.toggle("active");
+  menuBtn.setAttribute("aria-expanded", !isOpened);
+});
